67938db99421bbf62deaedb40c0ae1a77cbf1a57
Kirk Hayes (l0gan) <kirk.paul.hayes@gmail.com>
2014-06-25 16:06:41 -0400

    Update 1.3 - Sort and unique & remove punctuation marks

--- wordListParser.py
+++ wordListParser.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
-# Version 1.1
+# Version 1.3
 import sys
 from argparse import ArgumentParser
+import re
+import string
+from string import punctuation
+import os
 
 parser = ArgumentParser(description='Parse a file for words of a certain length')
 parser.add_argument('-f', '--file', default='', help='Dictionary file name.  (Run from directory where dictionary file is located)')
@@ -16,10 +20,8 @@ def main():
 	lines = []
 	words = []
 
-	## Get filename arg##
+	## Get args ##
 	filename = args.file
-	
-	## Ask user for number of chars to search for ##
 	char_count = args.char_count
 	char_count_string = str(char_count)
 	
@@ -28,15 +30,27 @@ def main():
 	
 	## New filename is the original filename + char_count ##
 	new_file = char_count_string + 'chars_' + filename
+	tmp_file = "tmpFile.txt"
 	
 	## Read in lines from filename ##
 	for line in open(filename):
 		for word in line.split():
+			word = word.rstrip('?.,!;:')
 			wordlength = len(word)
 			if str(wordlength) == char_count_string:	
-				with open(new_file, 'a') as f:
-					f.write(word + '\n') 
+				with open(tmp_file, 'a') as f:
+					f.write(word + ' ')
 					f.close()
+
+	## Sort and Unique ##
+	for line in open(tmp_file):
+		for word in sorted(set(line.split())):
+			with open(new_file, 'a') as f:
+				f.write(word + '\n')
+				f.close()
+	## Remove tmpFile ##
+	os.remove(tmp_file)
+
 	print ('Finished!  Check ' + new_file + ' for your new wordlist')
 
 if __name__ == '__main__':
