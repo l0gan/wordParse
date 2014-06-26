3c5727f046158f7dda5d14ac9cf22579bf425a51
Kirk Hayes (l0gan) <kirk.paul.hayes@gmail.com>
2014-06-26 14:46:43 -0400

    V 1.5 - Add -C option to allow searching for strings

--- README.txt
+++ README.txt
@@ -16,5 +16,5 @@ wordParse - Created by Kirk Hayes (l0gan)
 2.  Command Line Arguments:
 	-f <filename> or --file <filename>  -  'Dictionary file name.  (Run from directory where dictionary file is located)
 	-c <number> or --char_count <number>  -  'Number of characters to search for'
-	-b <letter or str> or --begins <letter or str>  -  'FUTURE -  Grab words that begin with a letter or letters'
-3.  Usage:  python wordParse.py -f sourcefile.txt -c 4  (Search sourcefile.txt for 4 character words and output to 4char_sourcefile.txt)
+	-C <letter or str> or --contains <letter or str>  -  'Grab words that contain a string of characters.  CASE SENSITIVE'
+3.  Usage:  python wordParse.py -f sourcefile.txt -c 4 -C the (Search sourcefile.txt for 4 character words that contain the string 'the' and output to output_the_4char_sourcefile.txt)
--- wordListParser.py
+++ wordListParser.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Version 1.3
+# Version 1.5
 import sys
 from argparse import ArgumentParser
 import re
@@ -10,48 +10,88 @@ import os
 parser = ArgumentParser(description='Parse a file for words of a certain length')
 parser.add_argument('-f', '--file', default='', help='Dictionary file name.  (Run from directory where dictionary file is located)')
 parser.add_argument('-c', '--char_count', default='', help='Number of characters to search for')
-parser.add_argument('-b', '--begins', default='', help='FUTURE -  Grab words that begin with a letter or letters')
+parser.add_argument('-C', '--contains', default='', help='Grab words that contain a string of characters.  CASE SENSITIVE')
+
+args = parser.parse_args()
+if not len(sys.argv) > 1:
+	parser.print_help()
+
+
+## Variables ##
+filename = args.file
+char_count = args.char_count
+char_count_string = str(char_count)
+contains = args.contains
+new_file = 'output_' + contains + '_' + char_count_string + 'chars_' + filename
+tmp_file = "tmpFile.txt"
+tmp_file2 = "tmpFile2.txt"
+
+def character_counter():
+	 for line in open(filename):
+                for word in line.split():
+                        word = word.rstrip('?.,!;:')
+                        wordlength = len(word)
+                        if str(wordlength) == char_count_string:
+                                with open(tmp_file, 'a') as f:
+                                        f.write(word + ' ')
+                                        f.close()
+
+def containing():
+	if len(char_count_string) > 0:
+		cont_file = tmp_file
+	else:
+		cont_file = filename
+	for line in open(cont_file):
+               	for word in line.split():
+                       	word = word.rstrip('?.,!;:')
+                       	wordcont = str(contains)
+                       	if wordcont in word:
+                               	with open(tmp_file2, 'a') as f:
+                                       	f.write(word + ' ')
+                                       	f.close()
+ 
+
+def sorter():
+## Sort and Unique ##
+	if len(contains) > 1:
+		sort_file = tmp_file2
+		try:
+			srt_file = open(sort_file)
+		except IOError:
+			print ('No words found that matched the string ' + str(contains))
+			exit()
+	elif len(char_count_string) > 0:
+		sort_file = tmp_file
+	else:
+		parser.print_help()
+		exit()
+	for line in open(sort_file):
+                for word in sorted(set(line.split())):
+                        with open(new_file, 'a') as f:
+                                f.write(word + '\n')
+                                f.close()
+        ## Remove tmpFile ##
+	os.remove(sort_file)
+
 
 def main():
-	args = parser.parse_args()
-	if not len(sys.argv) > 1:
-		parser.print_help()
-		return
-	lines = []
-	words = []
+        lines = []
+        words = []
 
-	## Get args ##
-	filename = args.file
-	char_count = args.char_count
-	char_count_string = str(char_count)
-	
-	## Print out filename and char_count ##
-	print ('Parsing ' + filename + ' for ' + char_count_string + ' characters')
-	
-	## New filename is the original filename + char_count ##
-	new_file = char_count_string + 'chars_' + filename
-	tmp_file = "tmpFile.txt"
-	
-	## Read in lines from filename ##
-	for line in open(filename):
-		for word in line.split():
-			word = word.rstrip('?.,!;:')
-			wordlength = len(word)
-			if str(wordlength) == char_count_string:	
-				with open(tmp_file, 'a') as f:
-					f.write(word + ' ')
-					f.close()
+        ## Print out filename and char_count ##
+        print ('Parsing ' + filename + ' for ' + char_count_string + ' characters')
 
-	## Sort and Unique ##
-	for line in open(tmp_file):
-		for word in sorted(set(line.split())):
-			with open(new_file, 'a') as f:
-				f.write(word + '\n')
-				f.close()
-	## Remove tmpFile ##
-	os.remove(tmp_file)
+        if len(char_count_string) > 0:
+                character_counter()
 
-	print ('Finished!  Check ' + new_file + ' for your new wordlist')
+        if len(contains) > 1:
+                containing()
+
+        sorter()
+
+        print ('Finished!  Check ' + new_file + ' for your new wordlist')
+
+
 
 if __name__ == '__main__':
 	main()
